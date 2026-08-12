a302acb054a1be0481a86b363b2154c752b7aa49
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-12 14:28:10 +0530

    Added MongoDB connection using Mongoose

---
+++ backend/user/src/config/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+const  connectDb= async()=>{
+
+    const url= process.env.MONGO_URI;
+
+    if(!url){
+
+        throw new Error("MONGO_URI is not defined in enviroment variable");
+    }
+
+    try{
+
+        await mongoose.connect(url,{
+
+            dbName:"SyncMesh",
+        } );
+
+
+        console.log("connected to mongodb");
+
+    }
+
+    catch(error){
+
+        console.log("Failed to connect to Mongodb", error);
+        process.exit(1);
+    }
+};
+
