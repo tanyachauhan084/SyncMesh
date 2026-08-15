c0669a7d5deb4b3c86536b0f3e68950a6a9f2344
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-15 13:44:41 +0530

    Initialized Redis client connection with type casting

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import { createClient } from "redis";
 
 dotenv.config();
 
 
 connectDb();
+
+export const redisClient= createClient({
+    url: process.env.REDIS_URL as string,
+});
+
+
+redisClient.connect()
+.then(()=> console.log("connected to redis"))
+.catch(console.error);
 
 
 const app= express();
