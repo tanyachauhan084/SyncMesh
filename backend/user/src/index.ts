0aa4603c1eca533e4e9bc7511e5e34bef04e7625
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-16 15:02:49 +0530

    Mounted user router on /api/v1 prefix

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
 
 dotenv.config();
 
@@ -19,6 +20,9 @@ redisClient.connect()
 
 
 const app= express();
+
+
+app.use("api/vi", userRoutes);
 
 const port= process.env.PORT;
 
