e9888c51775f6a2e482331a63da7b6ab0b37fc7c
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-18 20:57:26 +0530

    Added publishToQueue helper function

--- backend/user/src/config/rabbitmq.ts
+++ backend/user/src/config/rabbitmq.ts
@@ -27,3 +27,25 @@ export const connnectRabbitMQ= async()=>{
     }
 }
 
+
+
+
+
+
+
+export const publishToQueue= async(queueName: string, message: any)=>{
+
+    if(!channel){
+        console.log("Rabbitmq channel is not initialized");
+
+        return;
+    }
+
+    await channel.assertQueue(queueName, {durable: true});
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)),{
+
+        persistent: true
+    });
+
+};
