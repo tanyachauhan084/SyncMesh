4acb6f2ce9d8aeccf83a64e4a0a187c11f9db51d
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-19 20:39:19 +0530

    init mail service server

---
+++ backend/mail/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app= express()
+
+
+app.listen(process.env.PORT, ()=>{
+
+
+    console.log(`server is running on port: ${process.env.PORT}`)
+})
+
