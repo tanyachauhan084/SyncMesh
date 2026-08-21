472c4043bcb7a9ac0c63719f3618c00119351570
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-21 14:14:29 +0530

    Connected to RabbitMQ and configure OTP queue

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -5,3 +5,45 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const startSendOptConsumer= async()=>{
+
+    try {
+        
+        const connection= await amqp.connect({
+                protocol:"amqp",
+                hostname: process.env.RABBITMQ_HOST!,
+                port:5672,
+                username: process.env.RABBITMQ_USERNAME!,
+                
+
+                password: process.env.RABBITMQ_PASSWORD!
+
+        })
+
+
+
+        const channel= await connection.createChannel()
+
+
+
+
+        const queueName= "second-otp"
+
+
+
+        await channel.assertQueue(queueName, {durable:true});
+
+
+        console.log("Mail service consumer started, listening for otp emails");
+
+
+     
+    } catch (error) {
+        console.log("Failed to start rabbitmq consumer", error);
+    }
+}
+
+
+
+
+
