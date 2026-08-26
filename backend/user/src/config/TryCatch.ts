a71669d8467d63374175804bbf0887cec725c631
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-26 20:39:33 +0530

    added handler() in try block

--- backend/user/src/config/TryCatch.ts
+++ backend/user/src/config/TryCatch.ts
@@ -11,7 +11,7 @@ const TryCatch= (handler: RequestHandler):RequestHandler=>{
     return async(req:Request, res: Response, next: NextFunction)=>{
 
         try {
-            
+            await handler(req,res,next);
         } catch (error) {
 
 
