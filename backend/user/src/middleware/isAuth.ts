917c6c778cdd2b09ff58a984793e788487d383f3
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-31 19:49:05 +0530

    Add JWT authentication middleware

---
+++ backend/user/src/middleware/isAuth.ts
@@ -0,0 +1,67 @@
+import {type Request, type NextFunction, type Response } from "express";
+import jwt from "jsonwebtoken"
+import {type IUser} from "../model/User.js";
+import type { JwtPayload } from "jsonwebtoken";
+
+
+export interface AuthenticatedRequest extends Request{
+    user?: IUser| null;
+}
+
+export const isAuth= async(req:AuthenticatedRequest, res: Response, next: NextFunction): 
+
+
+Promise<void>=>{
+
+    try {
+        const authHeader= req.headers.authorization
+
+
+
+        if(!authHeader  || !authHeader.startsWith("Bearer ")){
+
+            res.status(401).json({
+
+                message:"Please login - NO auth header"
+            });
+
+
+
+            return;
+        }
+        
+        const token = authHeader.split(" ")[1];
+
+if (!token) {
+    res.status(401).json({
+        message: "Invalid authorization header"
+    });
+    return;
+}
+
+
+        const decodedValue= jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload
+
+        if(! decodedValue || !decodedValue.user){
+
+            res.status(401).json({
+
+                message: "Invalid token"
+            });
+
+
+            return;
+        }
+
+        req.user= decodedValue.user;
+        next();
+
+    } catch (error) {
+        
+
+        res.status(401).json({
+
+            message: "Please login - JWT error"
+        })
+    }
+}
