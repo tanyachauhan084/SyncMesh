a84ce0d8d61329d38e795e22b357f1f0bb420ba8
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-08-31 19:44:59 +0530

    add verifyuser route

--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,9 +1,12 @@
 import express from "express"
-import { loginUser } from "../controllers/user.js";
+import { loginUser, verifyuser } from "../controllers/user.js";
 
 const router= express.Router();
 
 
 router.post("/login", loginUser);
+
+
+router.post("/verify", verifyuser);
 
 export default router;
