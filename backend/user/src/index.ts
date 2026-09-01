13f37c0603b26cf153ca2e175e8291d61eb4a8d6
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-01 21:34:10 +0530

    Enable CORS middleware

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -4,6 +4,7 @@ import connectDb from "./config/db.js";
 import { createClient } from "redis";
 import userRoutes from "./routes/user.js";
 import { connnectRabbitMQ } from "./config/rabbitmq.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -26,6 +27,8 @@ redisClient.connect()
 const app= express();
 
 app.use(express.json());
+
+app.use(cors());
 
 
 app.use("/api/v1", userRoutes);
