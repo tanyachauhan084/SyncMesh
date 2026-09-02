f767bfc40f5568f364cb6f64ff7ab75010e2d571
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-02 21:38:59 +0530

    added express server

---
+++ backend/chat/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv"
+
+const app= express();
+
+dotenv.config();
+
+const port= process.env.PORT;
+
+app.listen(port, ()=>{
+
+console.log(`Serverr is running at ${port}`);
+})
