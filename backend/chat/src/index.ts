d059ea573d0cebdc9ae24eb0025b3acf6c5352d9
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-03 21:31:15 +0530

    Connected application to database

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv"
+import connectDb from "./config/db.js";
 
 const app= express();
 
 dotenv.config();
+
+connectDb();
 
 const port= process.env.PORT;
 
