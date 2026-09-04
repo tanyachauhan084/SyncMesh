d1b263d389b94b9bede9246104963c4e327cbcc6
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-04 20:21:41 +0530

    Added get all chats route

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { createNewChat } from "../controllers/chat.js";
+import { createNewChat, getAllChats } from "../controllers/chat.js";
 
 
 const router= express.Router();
 
 router.post("/chat/new", isAuth, createNewChat);
+router.get("/chat/all", isAuth, getAllChats);
+
 
 export default router;
