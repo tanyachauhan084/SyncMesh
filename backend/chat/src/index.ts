ca64f6ebc205dba88ce4996e6bd06899d7a88f41
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-04 20:23:26 +0530

    Mounted chat routes

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import connectDb from "./config/db.js";
+import chatRoutes from "./routes/chat.js";
 
 const app= express();
 
@@ -9,6 +10,8 @@ dotenv.config();
 connectDb();
 
 app.use(express.json());
+
+app.use("/api/v1", chatRoutes);
 
 const port= process.env.PORT;
 
