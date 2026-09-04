409cbfd8e5bdf3358fdf4827520491d36eaa4ed7
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-04 19:58:04 +0530

    Added get all chats functionality

--- backend/chat/src/controllers/chat.ts
+++ backend/chat/src/controllers/chat.ts
@@ -32,3 +32,30 @@ export const  createNewChat= TryCatch(async(req: AuthenticatedRequest, res)=>{
         chatId: newChat._id,
     })
 })
+
+
+
+export const getAllChats= TryCatch(async(req: AuthenticatedRequest, res)=>{
+
+    const userId= req.user?._id;
+
+    if(!userId){
+
+        res.status(400).json({
+
+            message:" Userid missing"
+        });
+
+        return;
+    }
+
+
+    const chats= await Chat.find({users: userId}).sort({updatedAt: -1});
+
+    const chatWithUserData= await Promise.all(
+        chats.map(async(chat)=>{
+
+            const otherUserId= chat.users.find((id)=> id! == userId);
+        })
+    )
+})
