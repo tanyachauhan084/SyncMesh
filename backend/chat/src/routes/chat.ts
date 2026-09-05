02714aa7090666a6508d68b0f1c57da090bf6016
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-05 19:23:01 +0530

    Added message upload route

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { createNewChat, getAllChats } from "../controllers/chat.js";
+import { createNewChat, getAllChats, sendMessage } from "../controllers/chat.js";
+import { upload } from "../middlewares/multer.js";
 
 
 const router= express.Router();
 
 router.post("/chat/new", isAuth, createNewChat);
 router.get("/chat/all", isAuth, getAllChats);
-
+router.post("/message", isAuth, upload.single('image'), sendMessage);
 
 export default router;
