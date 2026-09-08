669f438203bf5a7f61c7c1ef9eba0a9a77a54af9
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-08 21:42:59 +0530

    Added email verification page

---
+++ frontend/src/app/verify/page.tsx
@@ -0,0 +1,82 @@
+"use client"
+import { ArrowRight, Loader2, Lock } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useRef, useState } from 'react'
+
+
+const VerifyPage = () => {
+ 
+    const [loading, setLoading]= useState(false);
+    const [otp, setOtp]= useState<string[]>(["","","","","",""]);
+    const [error, setError]= useState<string>("");
+    const [resendLoading, setresendLoading]= useState(false);
+    const [timer, setTimer]= useState(60);
+    const inputRefs= useRef<Array<HTMLInputElement>|null>([]);
+    const router= useRouter();
+    const searchParams= useSearchParams();
+
+    const email: string= searchParams.get("email")|| "";
+
+    const handleSubmit= async()=>{}
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <div className="max-w-md w-full">
+        <div className="bg-gray-800 border border-gray-700 rounded-lg p-8">
+          <div className="text-center mb-8">
+            <div className="mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6">
+              <Lock size={40} className="text-white" />
+            </div>
+            <h1 className="text-4xl font-bold text-white mb-3">
+              Verify Your Email
+            </h1>
+            <p className="text-gray-300 text-lg">
+              We have sent a 6-digit code to your e
+            </p>
+            <p className='text-blue-400 font-medium'>{email}</p>
+          </div>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-300 mb-2"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-4 py-4 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400"
+                placeholder="Enter Your email address"
+                // value={email}
+                // onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <button
+               type="submit"
+              className="w-full bg-blue-600 text-white py-4 px-6 rounded-lg font-semibold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed"
+            //   disabled={loading}
+            >
+
+                  {loading ? (
+                <div className="flex items-center justify-center gap-2">
+                  <Loader2 className="w-5 h-5" />
+                  Sending Otp to your mail...
+                </div>
+              ) : (
+                <div className="flex items-center justify-center gap-2">
+                  <span>Send Verification Code</span>
+                  <ArrowRight className="w-5 h-5" />
+                </div>
+        )}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+ 
+}
+
+export default VerifyPage;
