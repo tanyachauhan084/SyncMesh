0bb6596779c17bcac6233741c48458173e1b7114
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-08 21:04:09 +0530

    corrected useRouter import path

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { ArrowRight, Mail } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useState } from "react";
 
