318063251e8ccd775bfff55b41b81004211bf5f0
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-13 19:43:22 +0530

    add back to login button

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ArrowRight, Loader2, Lock } from 'lucide-react';
+import { ArrowRight, ChevronLeft, Loader2, Lock } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useRef, useState } from 'react'
 import Cookies from 'js-cookie';
@@ -123,7 +123,14 @@ const VerifyPage = () => {
     <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
       <div className="max-w-md w-full">
         <div className="bg-gray-800 border border-gray-700 rounded-lg p-8">
-          <div className="text-center mb-8">
+          <div className="text-center mb-8 relative">
+
+    <button
+              className="absolute top-0 left-0 p-2 text-gray-300 hover:text-white"
+              onClick={() => router.push("/login")}
+            >
+              <ChevronLeft className="w-6 h-6" />
+            </button>
             <div className="mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6">
               <Lock size={40} className="text-white" />
             </div>
