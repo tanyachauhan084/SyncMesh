a4d61d158daeb7b6e5feae9dd57ab49bfae12cf6
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-21 20:54:56 +0530

    Use centralized user service URL

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -4,6 +4,7 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useRef, useState } from 'react'
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import { user_services } from '@/context/AppContext';
 
 const VerifyPage = () => {
  
@@ -77,7 +78,7 @@ const VerifyPage = () => {
 
   
     try {
-      const { data } = await axios.post('http://localhost:5000/api/v1/verify', {
+      const { data } = await axios.post(`${user_services}/api/v1/verify`, {
         email,
         otp: otpString,
       });
@@ -107,7 +108,7 @@ const VerifyPage = () => {
     setResendLoading(true);
     setError("");
     try {
-      const { data } = await axios.post('http://localhost:5000/api/v1/login', {
+      const { data } = await axios.post(`${user_services}/api/v1/login`, {
         email,
       });
         
