63081a61f0c840772380aa372bd8d2516653419b
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-21 22:57:47 +0530

    Enable CORS middleware

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import connectDb from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
+import cors from "cors";
 
 const app= express();
 
@@ -10,6 +11,8 @@ dotenv.config();
 connectDb();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/v1", chatRoutes);
 
