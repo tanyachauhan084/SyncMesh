f7e397c7f8919fedfaa0a45033117c85cef9ba82
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-22 22:44:13 +0530

    Track connected users and broadcast online users

--- backend/chat/src/config/socket.ts
+++ backend/chat/src/config/socket.ts
@@ -24,6 +24,17 @@ io.on("connection", (socket: Socket)=>{
 
     console.log("User connected", socket.id);
 
+    const userId= socket.handshake.query.userId as string | undefined;
+
+
+    if(userId  && userId!=="undefined"){
+
+        userSocketMap[userId]= socket.id
+        console.log(`User ${userId} mapped to soket ${socket.id}`)
+    }
+
+    io.emit("getOnlineUser", Object.keys(userSocketMap));
+
 
     socket.on("disconnect", ()=>{
 
