4c4e697760cb38fe7ba2aa7e86547aa59b482067
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-22 22:25:25 +0530

    Set up Socket.IO connection handling

--- backend/chat/src/config/socket.ts
+++ backend/chat/src/config/socket.ts
@@ -8,5 +8,37 @@ const app= express();
 
 const server= http.createServer(app);
 
+
+const io= new Server(server, {
+cors:{
+    origin:"*",
+    methods: ["GET", "POST"],
+
+},
+});
+
+
+const userSocketMap: Record<string, string>= {};
+
+io.on("connection", (socket: Socket)=>{
+
+    console.log("User connected", socket.id);
+
+
+    socket.on("disconnect", ()=>{
+
+        console.log("User Disconnected", socket.id);
+
+
+    });
+
+
+    socket.on("connect_error", (error)=>{
+
+        console.log("Socket Connection Error", error);
+    });
+});
+
+
 export {app, server};
 
