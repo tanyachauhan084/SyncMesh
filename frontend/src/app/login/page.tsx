d249259ac53c37c8e80c34bb5b6efdd2912ca6fc
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-09-25 13:35:09 +0530

    used user service

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRight, Loader2, Mail } from "lucide-react";
 import { redirect, useRouter } from "next/navigation";
 import axios from "axios";
 import { useState } from "react";
-import { useAppData } from "@/context/AppContext";
+import { useAppData, user_service } from "@/context/AppContext";
 import Loading from "@/components/Loading";
 import toast from "react-hot-toast";
 
@@ -22,7 +22,7 @@ const LoginPage = () => {
     setLoading(true);
 
     try {
-      const { data } = await axios.post(`http://localhost:5000/api/v1/login`, {
+      const { data } = await axios.post(`${user_service}/api/v1/login`, {
         email,
       });
 
